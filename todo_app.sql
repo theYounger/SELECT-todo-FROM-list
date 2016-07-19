8e82c3dfcbe5f9685014a2890c8ddbc1f7a655de
theYounger <kuyehara1@gmail.com>
2016-07-18 19:32:12 -1000

    start problem 10, having finished select and update problems

--- todo_app.sql
+++ todo_app.sql
@@ -12,23 +12,41 @@ CREATE DATABASE todo_app;
 
 --6 & 7
 CREATE TABLE tasks (
-  id  integer CONSTRAINT NOT NULL DEFAULT PRIMARY KEY,
-  title varchar(255) CONSTRAINT NOT NULL,
+  id SERIAL PRIMARY KEY,
+  title varchar(255) NOT NULL,
   description text,
-  created_at timestamp without time zone CONSTRAINT NOT NULL DEFAULT now(),
-  updated_at timestamp without time zone,
-  completed boolean CONSTRAINT NOT NULL DEFAULT false
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp,
+  completed boolean NOT NULL DEFAULT false
   );
 --8 Write queries to accomplish the following
 --i remove the column named completed
--- ALTER TABLE tasks DROP COLUMN completed;
--- --ii add a column to tasks named completed_at:timestamp, that may be NULL, and has a default value of NULL
--- ALTER TABLE tasks ADD COLUMN completed_at :timestamp;
--- --iii change the updated_at column to not allow NULL values, and have a default value of now()
--- ALTER TABLE tasks ALTER COLUMN completed_at SET DEFAULT NULL;
--- ALTER TABLE tasks ALTER COLUMN updated_at SET NOT NULL;
--- ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT now();
----iv
+ALTER TABLE tasks
+  DROP COLUMN completed;
+--ii add a column to tasks named completed_at:timestamp, that may be NULL, and has a default value of NULL
+ALTER TABLE tasks
+  ADD COLUMN completed_at timestamp with time zone DEFAULT NULL;
+--iii change the updated_at column to not allow NULL values, and have a default value of now()
+ALTER TABLE tasks
+  ALTER updated_at SET DEFAULT now();
+ALTER TABLE tasks
+  ALTER updated_at SET NOT NULL;
+--iv
+INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+--v
+INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation', DEFAULT, DEFAULT, DEFAULT);
+--vi
+SELECT title FROM tasks WHERE completed_at IS NULL;
+--vii
+UPDATE tasks
+  SET completed_at = now()
+  WHERE title = 'Study SQL';
+--viii
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
+--ix
+SELECT * FROM tasks ORDER BY created_at DESC;
+\d+ tasks;
+
 
 SELECT * FROM tasks;
 --end
